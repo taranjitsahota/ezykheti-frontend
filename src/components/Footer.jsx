5e56f3176790eda1c8c890e2cd8c27508efc802f
taranjitsahota <taranjit.sahota@gmail.com>
2025-04-16 00:04:34 +0530

    small error image name fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/images/logo/logo.png";
+import logo from "../assets/images/logo/Logo.png";
 import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 import { useTranslation } from "react-i18next";
 
