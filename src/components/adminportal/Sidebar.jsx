4d32366c98abd851ad33783a03cc611b6d6c8be3
taranjitsahota <taranjit.sahota@gmail.com>
2025-05-04 19:30:31 +0530

    small fix

--- src/components/adminportal/Sidebar.jsx
+++ src/components/adminportal/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/images/logo/Logo-icon.png";
+import logo from "../../assets/images/logo/Logo-Icon.png";
 import {
   LayoutDashboard,
   User,
